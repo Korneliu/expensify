9173c434bfbecf1c304e8d88ca8f6e3ab7fa871f
Korneliu <olbiton@gmail.com>
2020-11-24 23:26:45 -0800

    first commit, used boilerplate.v1

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+const template = (
+  <div>
+    <h1>Whatever</h1>
+  </div>
+)
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
+
