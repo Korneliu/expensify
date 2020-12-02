7b0179e4bfd929b9e8c4f9955d1fa83566dcae68
Korneliu <olbiton@gmail.com>
2020-12-01 22:47:56 -0800

    added ExpenseList and ExpenseListItem component

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 2000 }))
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 1999 }))
+//store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 
-
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
+ReactDOM.render(jsx, document.getElementById('app'))
 
