2e0359c67d68132d4605efb688746f70ad049e9a
Korneliu <olbiton@gmail.com>
2021-01-12 22:58:08 -0800

    added jest

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -79,7 +79,7 @@ export default class ExpenseForm extends React.Component {
             focused={this.state.calenderaFocused}
             onFocusChange={this.onFocusChange}
             numberOfMonths={1}
-            isOutsideRange={(day) => false}
+            isOutsideRange={() => false}
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
